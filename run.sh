4220c24f2dca95c84ecd860607dc9b39bf6fdca6
Cesar Perez <cperez@wnohang.com>
2017-01-15 00:47:22 +0100

    Added an initial version of the step

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Copyright (C) 2017 BigTrueData
+
+set -e
+
+version=$WERCKER_NODE_INSTALL_VERSION
+
+tarball="node-v${version}-linux-x64.tar.gz"
+url="https://nodejs.org/dist/v${version}/${tarball}"
+
+wget -O- ${url} | tar xzf - -C /usr/local --strip-components=1
+ln -s /usr/local/bin/node /usr/local/bin/nodejs
+
+exit 0
