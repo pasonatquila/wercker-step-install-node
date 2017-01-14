12affeb797938d7ed67abc9b4a310f0449273f65
Cesar Perez <cperez@wnohang.com>
2017-01-15 00:52:03 +0100

    Added double quoting to prevent globbing and word splitting

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ version=$WERCKER_NODE_INSTALL_VERSION
 tarball="node-v${version}-linux-x64.tar.gz"
 url="https://nodejs.org/dist/v${version}/${tarball}"
 
-wget -O- ${url} | tar xzf - -C /usr/local --strip-components=1
+wget -O- "${url}" | tar xzf - -C /usr/local --strip-components=1
 ln -s /usr/local/bin/node /usr/local/bin/nodejs
 
 exit 0
