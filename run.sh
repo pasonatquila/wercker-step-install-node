d7b539919258fe8601a62da36a7e8dd4a27e5bed
Cesar Perez <cperez@wnohang.com>
2017-01-16 12:14:26 +0100

    Corrected some little errors

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ main() {
   local version=$WERCKER_NODE_INSTALL_VERSION
   local tarball="node-v${version}-linux-x64.tar.gz"
 
-  wget -O- "https://nodejs.org/dist/v${version}/${tarball}" \\
+  wget -O- "https://nodejs.org/dist/v${version}/${tarball}" \
   | tar xzf - -C /usr/local --strip-components=1
 
   ln -s /usr/local/bin/node /usr/local/bin/nodejs
 }
 
-main
+main;
