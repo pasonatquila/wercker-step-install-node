2ee0d1f53d4d8dccf51d1dde5923dc70631b1cb5
Cesar Perez <cperez@wnohang.com>
2017-01-16 12:30:08 +0100

    Simplified the run.sh scrip

--- run.sh
+++ run.sh
@@ -3,14 +3,10 @@
 
 set -e
 
-main() {
-  local version=$WERCKER_NODE_INSTALL_VERSION
-  local tarball="node-v${version}-linux-x64.tar.gz"
+version=$WERCKER_NODE_INSTALL_VERSION
+tarball="node-v${version}-linux-x64.tar.gz"
 
-  wget -O- "https://nodejs.org/dist/v${version}/${tarball}" \
-  | tar xzf - -C /usr/local --strip-components=1
+wget -O- "https://nodejs.org/dist/v${version}/${tarball}" \
+| tar xzf - -C /usr/local --strip-components=1
 
-  ln -s /usr/local/bin/node /usr/local/bin/nodejs
-}
-
-main;
+ln -s /usr/local/bin/node /usr/local/bin/nodejs
