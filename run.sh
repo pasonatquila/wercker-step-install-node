5c3a01bd7997fb724cf5dfba960c07726c7c71ec
Cesar Perez <cperez@wnohang.com>
2017-01-18 13:16:55 +0100

    Added the ability to choose between wget and curl

--- run.sh
+++ run.sh
@@ -6,7 +6,17 @@ set -e
 version=$WERCKER_INSTALL_NODE_VERSION
 tarball="node-v${version}-linux-x64.tar.gz"
 
-wget -O- "https://nodejs.org/dist/v${version}/${tarball}" \
-| tar xzf - -C /usr/local --strip-components=1
+url="https://nodejs.org/dist/v${version}/${tarball}"
+
+if [ `which wget` ]; then
+  wget -O- $url
+
+elif [ `which curl` ]; then
+  curl $url
+
+else
+  fail "Cannot find a command suitable to get the package"
+
+fi | tar xzf - -C /usr/local --strip-components=1
 
 ln -s /usr/local/bin/node /usr/local/bin/nodejs
